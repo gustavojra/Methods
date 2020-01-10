65e0877519db5f250940ff00b5c21fa99cbb3205
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2020-01-10 17:54:35 -0500

    tensor printer

---
+++ ecCC/Restricted/water/printtensor.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def printtensor(T):
+    dims = np.shape(T)
+    rank = len(dims)
+    if rank == 4:
+        i_range = range(0,dims[0])
+        j_range = range(0,dims[1])
+        for i in i_range:
+            for j in j_range:
+                print('Page ({},{},*,*)\n'.format(i,j))
+                printmatrix(T[i,j,:,:])
+    if rank == 3:
+        i_range = range(0,dims[0])
+        for i in i_range:
+            print('Page ({},*,*)\n'.format(i))
+            printmatrix(T[i,:,:])
+    if rank == 2:
+        printmatrix(T)
+
+def printmatrix(M):
+    dims = np.shape(M)
+    rank = len(dims)
+    out = ' '*5
+    i_range = range(0,dims[0])
+    j_range = range(0,dims[1])
+    # header
+    for j in j_range:
+        out += '{:>11}    '.format(j)
+    out += '\n'
+    for i in i_range:
+        out += '{:3d}  '.format(i)
+        for j in j_range:
+            out += '{:< 9.7f}    '.format(M[i,j])
+        out += '\n'
+    print(out)
